301f3c121769b02e2681c506da3c136eb37ba350
Florian Orben <florian.orben@gmail.com>
2015-10-11 03:27:26 +0200

    clean up messy code
    use good commit messages from now on

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
+// prettybenchmark: format go benchmarks into tables
+//
+// Usage: Pipe your benchmark results into "pb"
+//    go test -bench=. [-benchmem] | pb
 package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"io"
 	"os"
@@ -15,58 +20,47 @@ import (
 )
 
 var (
-	r  = regexp.MustCompile(`\s+`)
-	r2 = regexp.MustCompile(`-\d+$`)
-	r3 = regexp.MustCompile(`(?i:)(Benchmark_?)`)
+	byWhitespace = regexp.MustCompile(`\s+`)
+	byRuns       = regexp.MustCompile(`-\d+$`)
+	byIterations = regexp.MustCompile(`(?i:)(Benchmark_?)`)
 )
 
-type benchLine struct {
-	Name         string
-	FnIterations int
-	Iterations   int
-	Speed        float64
-	Bps          int
-	Aps          int
-	Status       string
-}
+type (
+	Foo       struct{}
+	benchmark struct {
+		info    *benchmarkInfo
+		results *results
+	}
+	benchmarkInfo struct {
+		hasFnIterations bool
+		benchmemUsed    bool
+		suggestedTiming string
+	}
+	results map[string][]*result
+	result  struct {
+		Name         string
+		FnIterations int
+		Runs         int
+		Speed        float64
+		Bps          int
+		Aps          int
+	}
+)
 
-var lines [][]byte
+type sortByFnIterations []*result
+
+func (b sortByFnIterations) Len() int           { return len(b) }
+func (b sortByFnIterations) Swap(i, j int)      { b[i], b[j] = b[j], b[i] }
+func (b sortByFnIterations) Less(i, j int) bool { return b[i].FnIterations < b[j].FnIterations }
+
+var (
+	lines [][]byte
+	table *termtables.Table
+	bench *benchmark
+)
 
 func main() {
-	// old := os.Stdout // keep backup of the real stdout
-	// r, w, err := os.Pipe()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// os.Stdout = w
-	//
-	// // print() // fails if called here "fatal error: all goroutines are asleep - deadlock"
-	//
-	// outC := make(chan bytes.Buffer)
-	// // copy the output in a separate goroutine so printing can't block indefinitely
-	// go func() {
-	// 	var buf bytes.Buffer
-	// 	io.Copy(&buf, r)
-	// 	outC <- buf
-	// }()
-	//
-	// // back to normal state
-	// w.Close()
-	// os.Stdout = old // restoring the real stdout
-	// out := <-outC
-	//
-	// // reading our temp stdout
-	//
-	// //fmt.Print(out)
-	//
-	// var reader *bufio.Reader
-	// if len(out.Bytes()) > 0 {
-	// 	reader = bufio.NewReader(&out)
-	// } else {
-	// 	reader = bufio.NewReader(os.Stdin)
-	// }
 
-	fmt.Print("\n")
 	reader := bufio.NewReader(os.Stdin)
 	quit := make(chan bool)
 	go loading(quit)
@@ -83,195 +77,259 @@ func main() {
 	}
 	close(quit)
 
-	benchMap := getNameToBenchmarkLinesMap(lines)
-	hasFnIterations, speedTblHeader := getInfoForLines(benchMap)
+	bench = newBenchmark(lines)
 
-	table := termtables.CreateTable()
-	if hasFnIterations {
-		table.AddHeaders(bold("Name"), bold("Iterations"), bold("No."), bold(speedTblHeader+"/op"), bold("B/op"), bold("allocations/op"), "")
-	} else {
-		table.AddHeaders(bold("Name"), bold("No."), bold(speedTblHeader+"/op"), bold("B/op"), bold("allocations/op"), "")
-	}
+	table = termtables.CreateTable()
+	addTableHeader(table)
+	addTableBody(table)
 
-	i := len(benchMap)
-	sorted := make([]string, 0, len(benchMap))
-	for name := range benchMap {
-		sorted = append(sorted, name)
-	}
-	sort.Sort(sort.StringSlice(sorted))
-
-	for _, benchName := range sorted {
-		benchLines := benchMap[benchName]
-
-		for j, b := range benchLines {
-			var name string
-			if j == 0 {
-				name = bold(b.Name)
-			}
-
-			if hasFnIterations {
-				fnIterations := strconv.Itoa(b.FnIterations)
-
-				if fnIterations == "-1" {
-					fnIterations = ""
-				}
-
-				table.AddRow(name, fnIterations, b.Iterations, b.Speed, b.Bps, b.Aps, b.Status)
-			} else {
-				table.AddRow(name, b.Iterations, b.Speed, b.Bps, b.Aps, b.Status)
-			}
-		}
-
-		i--
-		if i > 0 {
-			table.AddSeparator()
-		}
-	}
-
-	table.SetAlign(termtables.AlignLeft, 1)
-	table.SetAlign(termtables.AlignRight, 2)
-	table.SetAlign(termtables.AlignRight, 3)
-	table.SetAlign(termtables.AlignRight, 4)
-	table.SetAlign(termtables.AlignRight, 5)
-	table.SetAlign(termtables.AlignRight, 6)
-
-	fmt.Print("\r")
-	footer := strings.TrimSpace(string(lines[len(lines)-1]))
-	footer = strings.Replace(footer, "\t", "     ", -1)
-	padLeft := (table.Style.Width - len(footer)) / 4
-	for i := 0; i < padLeft; i++ {
-		fmt.Print(bold(" "))
-	}
-	fmt.Print(bold(footer))
-	fmt.Print("\n")
-	for i := 0; i < padLeft-1; i++ {
-		fmt.Print(bold(" "))
-	}
-	fmt.Print(bold("+"))
-	for range footer {
-		fmt.Print(bold("-"))
-	}
-	fmt.Print(bold("+"))
-	fmt.Print("\n")
-
+	fmt.Print("\r \n")
 	fmt.Println(table.Render())
+	fmt.Println(footer())
 }
 
-func getNameToBenchmarkLinesMap(lines [][]byte) map[string][]*benchLine {
-	benchMap := make(map[string][]*benchLine)
+func newBenchmark(l [][]byte) *benchmark {
+	results := newResults(l)
+	return &benchmark{
+		info:    newBenchmarkInfo(results),
+		results: results,
+	}
+}
 
-	for _, l := range lines[1 : len(lines)-1] {
-		bl := newbenchLineFromString(string(l))
-		if _, ok := benchMap[bl.Name]; !ok {
-			benchMap[bl.Name] = make([]*benchLine, 0)
+func newResults(l [][]byte) *results {
+	benchMap := make(results)
+
+	for _, l := range l[1 : len(l)-1] {
+		bl := newResult(l)
+		if bl != nil {
+			if _, ok := benchMap[bl.Name]; !ok {
+				benchMap[bl.Name] = make([]*result, 0)
+			}
+			benchMap[bl.Name] = append(benchMap[bl.Name], bl)
 		}
-		benchMap[bl.Name] = append(benchMap[bl.Name], bl)
 	}
 
-	return benchMap
+	for _, r := range benchMap {
+		sort.Sort(sortByFnIterations(r))
+	}
+
+	return &benchMap
 }
 
-func newbenchLineFromString(s string) *benchLine {
-	//qwe := "Benchmark_Sort_1000-8                	   20000	     84093 ns/op	      32 B/op	       1 allocs/op"
+func newResult(b []byte) *result {
+	var (
+		name   string
+		fnIter int
+		bps    int
+		aps    int
+		err    error
+		iter   int
+		speed  float64
+	)
 
-	y := r.Split(s, -1)
-	x := r2.ReplaceAllString(y[0], "")
-	c := r3.ReplaceAllString(x, "")
-	lastIndex := strings.LastIndex(c, "_")
-
-	var name string
-	var fnIter int
+	s := string(b)
+	parts := byWhitespace.Split(s, -1)
+	nameRuns := byRuns.ReplaceAllString(parts[0], "")
+	nameIterations := byIterations.ReplaceAllString(nameRuns, "")
+	lastIndex := strings.LastIndex(nameIterations, "_")
 
 	if lastIndex > -1 {
-		name = c[:lastIndex]
-		fnIter, _ = strconv.Atoi(c[lastIndex+1:])
+		name = nameIterations[:lastIndex]
+		fnIter, _ = strconv.Atoi(nameIterations[lastIndex+1:])
 	} else {
-		name = c
+		name = nameIterations
 		fnIter = -1
 	}
 
-	iter, err := strconv.Atoi(y[1])
+	//just print the line if it doesn't have the correct format
+	if len(parts) < 4 {
+		fmt.Println(s)
+		return nil
+	}
+
+	iter, err = strconv.Atoi(parts[1])
 	if err != nil {
 		iter = -1
 	}
-	speed, err := strconv.ParseFloat(y[2], 64)
+	speed, err = strconv.ParseFloat(parts[2], 64)
 	if err != nil {
 		speed = -1
 	}
-	bps, err := strconv.Atoi(y[4])
-	if err != nil {
+
+	if len(parts) > 5 {
+		bps, err = strconv.Atoi(parts[4])
+		if err != nil {
+			bps = -1
+		}
+		aps, err = strconv.Atoi(parts[6])
+		if err != nil {
+			aps = -1
+		}
+	} else {
+
+		//without benchmem
 		bps = -1
-	}
-	aps, err := strconv.Atoi(y[6])
-	if err != nil {
 		aps = -1
 	}
 
-	return &benchLine{
+	return &result{
 		Name:         name,
 		FnIterations: fnIter,
-		Iterations:   iter,
+		Runs:         iter,
 		Speed:        speed,
 		Bps:          bps,
 		Aps:          aps,
 	}
 }
 
-func getInfoForLines(bm map[string][]*benchLine) (bool, string) {
-	var slowest float64
-	var hasIterations = false
-	var speedTblHeader = "ns"
+func newBenchmarkInfo(r *results) *benchmarkInfo {
+	var (
+		slowest         float64
+		hasFnIterations bool
+		benchmemUsed    bool
+		suggestedTiming string
+	)
+	suggestedTiming = "ns"
 
-	for _, bl := range bm {
-
-		bestSpeedPerLine := float64(-1)
-		bestLine := 0
-		worstLine := 0
-
-		for i, l := range bl {
+	for _, bl := range *r {
+		for _, l := range bl {
 
 			if l.FnIterations > -1 {
-				hasIterations = true
+				hasFnIterations = true
+			}
+
+			if l.Aps > -1 && l.Bps > -1 {
+				benchmemUsed = true
 			}
 
 			if slowest < l.Speed {
 				slowest = l.Speed
-				worstLine = i
-			}
-
-			if bestSpeedPerLine == -1 || bestSpeedPerLine > l.Speed {
-				bestSpeedPerLine = l.Speed
-				bestLine = i
 			}
 		}
-
-		bl[worstLine].Status = red("✘")
-		bl[bestLine].Status = green("✔")
 	}
 
 	switch {
 	case slowest <= 1e3:
-		speedTblHeader = "ns"
+		suggestedTiming = "ns"
 	case slowest > 1e3 && slowest <= 1e6:
-		speedTblHeader = "µs"
-		updateSpeedVals(bm, float64(1e3))
+		suggestedTiming = "µs"
+		updateSpeedVals(r, float64(1e3))
 	case slowest > 1e6 && slowest <= 1e9:
-		speedTblHeader = "ms"
-		updateSpeedVals(bm, float64(1e6))
+		suggestedTiming = "ms"
+		updateSpeedVals(r, float64(1e6))
 	case slowest > 1e9:
-		speedTblHeader = "s"
-		updateSpeedVals(bm, float64(1e9))
+		suggestedTiming = "s"
+		updateSpeedVals(r, float64(1e9))
 	}
 
-	return hasIterations, speedTblHeader
+	return &benchmarkInfo{hasFnIterations, benchmemUsed, suggestedTiming}
 }
 
-func updateSpeedVals(bm map[string][]*benchLine, f float64) {
-	for _, bl := range bm {
+func updateSpeedVals(r *results, f float64) {
+	for _, bl := range *r {
 		for _, l := range bl {
 			l.Speed = l.Speed / f
 		}
 	}
+}
+
+func footer() string {
+
+	lastLine := bytes.Replace(
+		bytes.TrimSpace(lines[len(lines)-1]),
+		[]byte{9},
+		[]byte{32, 32, 32, 32, 32},
+		-1,
+	)
+	footer := make([]byte, 0, len(lastLine)*2+1)
+
+	footer = append(
+		footer,
+		lastLine...,
+	)
+
+	footer = append(footer, byte(10))
+	footer = append(footer, byte(43))
+	for i := 0; i < len(lastLine)-2; i++ {
+		footer = append(footer, byte(45))
+	}
+	footer = append(footer, byte(43))
+
+	return bold(string(footer))
+}
+
+func addTableHeader(t *termtables.Table) {
+	if bench.info.benchmemUsed {
+		if bench.info.hasFnIterations {
+			t.AddHeaders(bold("Name"), bold("Iterations"), bold("Runs"), bold(bench.info.suggestedTiming+"/op"), bold("B/op"), bold("allocations/op"), "")
+		} else {
+			t.AddHeaders(bold("Name"), bold("Runs"), bold(bench.info.suggestedTiming+"/op"), bold("B/op"), bold("allocations/op"), "")
+		}
+	} else {
+		if bench.info.hasFnIterations {
+			t.AddHeaders(bold("Name"), bold("Iterations"), bold("Runs"), bold(bench.info.suggestedTiming+"/op"), "")
+		} else {
+			t.AddHeaders(bold("Name"), bold("Runs"), bold(bench.info.suggestedTiming+"/op"), "")
+		}
+	}
+}
+
+func addTableBody(t *termtables.Table) {
+	i := len(*bench.results)
+	sorted := make([]string, 0, i)
+	for name := range *bench.results {
+		sorted = append(sorted, name)
+	}
+	sort.Sort(sort.StringSlice(sorted))
+
+	for _, benchName := range sorted {
+		results := (*bench.results)[benchName]
+
+		for j, b := range results {
+			var name string
+			if j == 0 {
+				name = bold(b.Name)
+			}
+
+			if bench.info.benchmemUsed {
+				if bench.info.hasFnIterations {
+					fnIterations := strconv.Itoa(b.FnIterations)
+
+					if fnIterations == "-1" {
+						fnIterations = ""
+					}
+
+					t.AddRow(name, fnIterations, b.Runs, b.Speed, b.Bps, b.Aps, "⬅")
+				} else {
+					t.AddRow(name, b.Runs, b.Speed, b.Bps, b.Aps, "⬅")
+				}
+			} else {
+				if bench.info.hasFnIterations {
+					fnIterations := strconv.Itoa(b.FnIterations)
+
+					if fnIterations == "-1" {
+						fnIterations = ""
+					}
+
+					t.AddRow(name, fnIterations, b.Runs, b.Speed, "⬅")
+				} else {
+					t.AddRow(name, b.Runs, b.Speed, "⬅")
+				}
+			}
+		}
+
+		i--
+		if i > 0 {
+			t.AddSeparator()
+		}
+	}
+
+	t.SetAlign(termtables.AlignLeft, 1)
+	t.SetAlign(termtables.AlignRight, 2)
+	t.SetAlign(termtables.AlignRight, 3)
+	t.SetAlign(termtables.AlignRight, 4)
+	t.SetAlign(termtables.AlignRight, 5)
+	t.SetAlign(termtables.AlignRight, 6)
 }
 
 func loading(q chan bool) {
@@ -295,16 +353,4 @@ func loading(q chan bool) {
 
 func bold(s string) string {
 	return fmt.Sprintf("\033[1m%s\033[0m", s)
-}
-
-func green(s string) string {
-	return fmt.Sprintf("\033[32m%s\033[0m", s)
-}
-
-func blue(s string) string {
-	return fmt.Sprintf("\033[34m%s\033[0m", s)
-}
-
-func red(s string) string {
-	return fmt.Sprintf("\033[31m%s\033[0m", s)
 }
